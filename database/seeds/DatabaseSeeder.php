e03c4c857acd9e76648a95430ee72458c54f055f
Miles Dragan <miloszdragan@openmailbox.org>
2015-10-08 11:37:47 +0200

    Seed user 'testy@test.com' with 'secret' for convenience

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,19 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        /**
+         * Seed, along others, one default user with
+         * known name
+         */
+        DB::table('users')->insert([
+            'name' => 'testy',
+            'email' => 'testy@test.com',
+            'password' => bcrypt('secret'),
+            'created_at' => '2015-10-08 09:00:00',
+            'updated_at' => '2015-10-08 09:00:00',
+            'remember_token' => str_random(10),
+        ]);
+
         $this->call(UserTableSeeder::class);
         $this->call(ReviewTableSeeder::class);
 
