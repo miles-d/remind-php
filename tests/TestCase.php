388c27a7e9c647c2ccfc4f7c4345ab2c8bf8f4bc
Milosz Dragan <miloszdragan@openmailbox.org>
2016-12-21 22:01:00 +0100

    test setup
    add empty test in TestCase so phpunit does not complain about empty test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      *
      * @var string
      */
-    protected $baseUrl = 'http://localhost';
+    protected $baseUrl = 'http://localhost:8000';
 
     /**
      * Creates the application.
@@ -21,5 +21,10 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
+    }
+
+    public function testFoo()
+    {
+        $this->assertTrue(true);
     }
 }
