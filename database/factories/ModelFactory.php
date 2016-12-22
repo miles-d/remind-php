f8e5227154173140567395108192ff9e915ba1eb
Milosz Dragan <miloszdragan@openmailbox.org>
2016-12-22 17:21:41 +0100

    update item levels in factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
-
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
@@ -25,7 +14,7 @@ $factory->define(App\ReviewItem::class, function (Faker\Generator $faker) {
         'user_id' => rand(1, 5),
         'last_review_date' => $faker->dateTimeBetween('-5 months', 'today')->format('Y-m-d'),
         'next_review_date' => $faker->dateTimeBetween('-5 months', '5 months')->format('Y-m-d'),
-        'level' => rand(0, 5),
+        'level' => rand(0, 7),
         'description' => $faker->sentence(3),
         'mastered' => false,
     ];
